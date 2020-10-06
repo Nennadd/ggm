f8d67990477d7eaa93d60be69db41294c6500327
Nennadd <nenadj372@gmail.com>
2020-10-06 16:55:00 +0200

    improving query

--- backend/utils/QueryBuilder.js
+++ backend/utils/QueryBuilder.js
@@ -1,10 +1,10 @@
-const mssql = require("mssql");
-const pool = require("./database");
+const { getConnection, sql } = require("./database");
 
 class Query {
   static async getAll() {
-    const db = await pool;
-    const result = await db.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier, RDR1.Price, 
+    const pool = await getConnection();
+    const request = await pool.request();
+    return await request.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier, RDR1.Price, 
     count(RDR1.ItemCode) as ArticleInOrders, OITM.onHand, 
     
     (SELECT sum(RDR1.Price) FROM RDR1 
@@ -25,42 +25,41 @@ class Query {
     JOIN OCRD on OITM.CardCode = OCRD.CardCode 
     WHERE OCRD.CardType = 'S'
     GROUP BY OITM.ItemCode, OITM.ItemName, OCRD.CardName, RDR1.Price, OITM.onHand`);
-
-    return result;
   }
 
   static async searchByDate(data) {
     const { dateFrom, dateTo } = data;
-    const request = new mssql.Request(await pool);
+    const pool = await getConnection();
+    const request = await pool.request();
 
-    request.input("dateFrom", mssql.VarChar, dateFrom);
-    request.input("dateTo", mssql.VarChar, dateTo);
+    request.input("dateFrom", sql.VarChar, dateFrom);
+    request.input("dateTo", sql.VarChar, dateTo);
 
-    return await request.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier, 
-    RDR1.Price, count(RDR1.ItemCode) as ArticleInOrders, OITM.onHand, 
+    return await request.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier,
+    RDR1.Price, count(RDR1.ItemCode) as ArticleInOrders, OITM.onHand,
 
-    (SELECT sum(RDR1.Price) FROM RDR1 JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-    JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-    JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-    WHERE OCRD.CardType = 'S' AND ORDR.DocDate between @dateFrom AND @dateTo) as TotalPrice, 
-              
-    (SELECT count(RDR1.ItemCode) FROM RDR1 JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-    JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-    JOIN OCRD on OITM.CardCode = OCRD.CardCode 
+    (SELECT sum(RDR1.Price) FROM RDR1 JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+    JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+    JOIN OCRD on OITM.CardCode = OCRD.CardCode
+    WHERE OCRD.CardType = 'S' AND ORDR.DocDate between @dateFrom AND @dateTo) as TotalPrice,
+
+    (SELECT count(RDR1.ItemCode) FROM RDR1 JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+    JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+    JOIN OCRD on OITM.CardCode = OCRD.CardCode
     WHERE OCRD.CardType = 'S' AND ORDR.DocDate between @dateFrom AND @dateTo) as SumOfArticles,
-    
-    (SELECT (cast(count(RDR1.ItemCode) as float) / cast(count(DISTINCT RDR1.ItemCode) as float)) 
-    FROM RDR1 
-    JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-    JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-    JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-    WHERE OCRD.CardType = 'S' AND ORDR.DocDate between @dateFrom AND @dateTo) as AvgArticleByOrder 
-    
-    FROM OITM 
-    JOIN RDR1 on OITM.ItemCode = RDR1.ItemCode 
-    JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-    JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-    
+
+    (SELECT (cast(count(RDR1.ItemCode) as float) / cast(count(DISTINCT RDR1.ItemCode) as float))
+    FROM RDR1
+    JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+    JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+    JOIN OCRD on OITM.CardCode = OCRD.CardCode
+    WHERE OCRD.CardType = 'S' AND ORDR.DocDate between @dateFrom AND @dateTo) as AvgArticleByOrder
+
+    FROM OITM
+    JOIN RDR1 on OITM.ItemCode = RDR1.ItemCode
+    JOIN OCRD on OITM.CardCode = OCRD.CardCode
+    JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+
     WHERE OCRD.CardType = 'S' AND ORDR.DocDate between @dateFrom AND @dateTo
     GROUP BY OITM.ItemCode, OITM.ItemName, OCRD.CardName, RDR1.Price, OITM.onHand`);
   }
@@ -69,9 +68,9 @@ class Query {
     if (supplier === "all") {
       return await this.getAll();
     } else {
-      const request = new mssql.Request(await pool);
-
-      request.input("supplier", mssql.VarChar, supplier);
+      const pool = await getConnection();
+      const request = await pool.request();
+      request.input("supplier", sql.VarChar, supplier);
 
       return await request.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier,
           RDR1.Price, count(RDR1.ItemCode) as ArticleInOrders, OITM.onHand,
@@ -97,62 +96,70 @@ class Query {
   }
 
   static async searchByPayment(payment) {
-    const db = await pool;
     if (payment === "all") {
       return await this.getAll();
     } else {
-      return await db.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier, RDR1.Price, OITM.onHand, count(RDR1.ItemCode)  as ArticleInOrders,
-                    
-      (SELECT sum(RDR1.Price) FROM RDR1 JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-      JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-      JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry 
-      WHERE OCRD.CardType = 'S' AND [@BOB_ORDR].Zahlbetrag ${payment} ORDR.DocTotal ) as TotalPrice, 
-                            
-      (SELECT count(RDR1.ItemCode) FROM RDR1 JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-      JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-      JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry 
+      const pool = await getConnection();
+      const request = await pool.request();
+      // request.input("payment", sql.VarChar, payment);
+      return await request.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier, RDR1.Price, OITM.onHand, count(RDR1.ItemCode)  as ArticleInOrders,
+
+      (SELECT sum(RDR1.Price) FROM RDR1 JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+      JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+      JOIN OCRD on OITM.CardCode = OCRD.CardCode
+      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry
+      WHERE OCRD.CardType = 'S' AND [@BOB_ORDR].Zahlbetrag ${payment} ORDR.DocTotal ) as TotalPrice,
+
+      (SELECT count(RDR1.ItemCode) FROM RDR1 JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+      JOIN OCRD on OITM.CardCode = OCRD.CardCode
+      JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry
       WHERE OCRD.CardType = 'S' AND [@BOB_ORDR].Zahlbetrag ${payment} ORDR.DocTotal ) as SumOfArticles,
-      (SELECT (cast(count(RDR1.ItemCode) as float) / cast(count(DISTINCT RDR1.ItemCode) as float)) 
-      FROM RDR1 
-      JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-      JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-      JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry 
+      (SELECT (cast(count(RDR1.ItemCode) as float) / cast(count(DISTINCT RDR1.ItemCode) as float))
+      FROM RDR1
+      JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+      JOIN OCRD on OITM.CardCode = OCRD.CardCode
+      JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry
       WHERE OCRD.CardType = 'S' AND [@BOB_ORDR].Zahlbetrag ${payment} ORDR.DocTotal) as AvgArticleByOrder
-      FROM OITM 
-      JOIN RDR1 on OITM.ItemCode = RDR1.ItemCode 
-      JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-      JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry 
-      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry 
-                  
-      WHERE OCRD.CardType = 'S' AND [@BOB_ORDR].Zahlbetrag ${payment} ORDR.DocTotal 
+      FROM OITM
+      JOIN RDR1 on OITM.ItemCode = RDR1.ItemCode
+      JOIN OCRD on OITM.CardCode = OCRD.CardCode
+      JOIN ORDR on ORDR.DocEntry = RDR1.DocEntry
+      JOIN [@BOB_ORDR] on ORDR.DocEntry = [@BOB_ORDR].DocEntry
+
+      WHERE OCRD.CardType = 'S' AND [@BOB_ORDR].Zahlbetrag ${payment} ORDR.DocTotal
       GROUP BY OITM.ItemCode, OITM.ItemName, OCRD.CardName, RDR1.Price, OITM.onHand`);
     }
   }
 
   static async searchByItemCodeOrName(data) {
-    const { itemCode, itemName } = data;
-    const db = await pool;
-    return await db.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier, 
-        RDR1.Price, count(RDR1.ItemCode) as ArticleInOrders, OITM.onHand, 
+    const { itemCode, itemName } = await data;
 
-        (SELECT sum(RDR1.Price) FROM RDR1 JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-        WHERE OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%') as TotalPrice, 
-        
-        (SELECT count(RDR1.ItemCode) FROM RDR1 JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-        JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-        WHERE OCRD.CardType = 'S' AND OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%' ) as SumOfArticles, 
+    const pool = await getConnection();
+    const request = await pool.request();
 
-        (SELECT (cast(count(RDR1.ItemCode) as float) / cast(count(DISTINCT RDR1.ItemCode) as float)) 
-        FROM RDR1 
-        JOIN OITM on OITM.ItemCode = RDR1.ItemCode 
-        JOIN OCRD on OITM.CardCode = OCRD.CardCode 
-        WHERE OCRD.CardType = 'S' AND OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%') as AvgArticleByOrder
-        
-        FROM OITM JOIN RDR1 on OITM.ItemCode = RDR1.ItemCode 
-        JOIN OCRD on OITM.CardCode = OCRD.CardCode 
+    request.input("itemCode", sql.VarChar, itemCode);
+    request.input("itemName", sql.VarChar, itemName);
+
+    return await request.query(`SELECT OITM.ItemCode, OITM.ItemName, OCRD.CardName as Supplier,
+        RDR1.Price, count(RDR1.ItemCode) as ArticleInOrders, OITM.onHand,
+
+        (SELECT sum(RDR1.Price) FROM RDR1 JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+        WHERE OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%' ) as TotalPrice,
+
+        (SELECT count(RDR1.ItemCode) FROM RDR1 JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+        JOIN OCRD on OITM.CardCode = OCRD.CardCode
+        WHERE OCRD.CardType = 'S' AND OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%' ) as SumOfArticles,
+
+        (SELECT (cast(count(RDR1.ItemCode) as float) / cast(count(DISTINCT RDR1.ItemCode) as float))
+        FROM RDR1
+        JOIN OITM on OITM.ItemCode = RDR1.ItemCode
+        JOIN OCRD on OITM.CardCode = OCRD.CardCode
+        WHERE OCRD.CardType = 'S' AND OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%' ) as AvgArticleByOrder
+
+        FROM OITM JOIN RDR1 on OITM.ItemCode = RDR1.ItemCode
+        JOIN OCRD on OITM.CardCode = OCRD.CardCode
         WHERE OCRD.CardType = 'S' AND OITM.ItemCode LIKE '%${itemCode}%' OR OITM.ItemName LIKE '%${itemName}%'
         GROUP BY OITM.ItemCode, OITM.ItemName, OCRD.CardName, RDR1.Price, OITM.onHand`);
   }
